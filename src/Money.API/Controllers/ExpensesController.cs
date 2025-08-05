8f78170f414300a441fd3f0baf4db0bcc148d598
Lucas Wojcik <lucaswojcikk@gmail.com>
2025-08-05 20:15:23 -0300

    implementação do endpoint POST para cadastro de despesas

---
+++ src/Money.API/Controllers/ExpensesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Money.Domain.Entities;
+using Money.Infrastructure.Data;
+
+namespace Money.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ExpensesController : ControllerBase
+{
+    private readonly MoneyDbContext _context;
+
+    public ExpensesController(MoneyDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: /api/expenses
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Expense>>> GetAll()
+    {
+        var expenses = await _context.Expenses.ToListAsync();
+        return Ok(expenses);
+    }
+
+    // POST: /api/expenses
+    [HttpPost]
+    public async Task<ActionResult<Expense>> Create([FromBody] Expense expense)
+    {
+        _context.Expenses.Add(expense);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetAll), new { id = expense.Id }, expense);
+    }
+}
