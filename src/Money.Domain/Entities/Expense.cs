d857c8f152d13fb43d2703d37f914d132de86ed6
Lucas Wojcik <lucaswojcikk@gmail.com>
2025-08-05 19:42:07 -0300

    Create Expense entity in Domain layer
    - Add Expense.cs class under Entities
    - Fields: Id, UserId, Description, Amount, Date, Category

---
+++ src/Money.Domain/Entities/Expense.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money.Domain.Entities
+{
+    internal class Expense
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid UserId { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
