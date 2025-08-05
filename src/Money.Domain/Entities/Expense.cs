ff9bbad2fba4589f9076bc53130b7b664fe46c68
Lucas Wojcik <lucaswojcikk@gmail.com>
2025-08-05 19:48:29 -0300

    Add MoneyDbContext with Expense entity
    - Created MoneyDbContext in Infrastructure layer
    - Registered DbSet<Expense> for future database mapping
    - Linked Infrastructure to Domain project

--- src/Money.Domain/Entities/Expense.cs
+++ src/Money.Domain/Entities/Expense.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Money.Domain.Entities
 {
-    internal class Expense
+    public class Expense
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
---
+++ src/Money.Infrastructure/Data/MoneyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Money.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Money.Infrastructure.Data;
+
+public class MoneyDbContext : DbContext
+{
+    public MoneyDbContext(DbContextOptions<MoneyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Expense> Expenses { get; set; }
+}
