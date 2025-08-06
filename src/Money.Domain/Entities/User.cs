fd86c104706ec47b612601386142d5f458d42309
Lucas Wojcik <lucaswojcikk@gmail.com>
2025-08-05 21:01:04 -0300

    feat(auth): create User entity with basic fields and relationship to Expense

---
+++ src/Money.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Money.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+}
